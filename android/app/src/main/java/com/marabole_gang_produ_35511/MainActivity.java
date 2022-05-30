a0e130075e23f2639e4ad1d996c57db7324eee0e
Crowdbotics <team@crowdbotics.com>
2022-05-30 21:28:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/marabole_gang_produ_35511/MainActivity.java
@@ -0,0 +1,15 @@
+package com.marabole_gang_produ_35511;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "marabole_gang_produ_35511";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "marabole_gang_produ_35511",
+  url: "https://marabole_gang_produ_35511.botics.co",
+  api: "https://marabole_gang_produ_35511.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
